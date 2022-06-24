578474bd52d744cfac463b771a68e3472474b614
Shubhay S. Shah <69692903+TechnoXtremeDevZ@users.noreply.github.com>
2022-06-24 15:43:15 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-﻿function preload() {
+var status = "";
+
+function preload() {
 
 }
 
 function setup() {
     canvas = createCanvas(290, 290);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
 }
 
 function draw() {
+    image(video, 0, 0, 290, 290);
+}
+
+function start() {
+    detector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model is Loaded");
+    status = true;
 
 }
